c527de08d0ce59b260c7e6783e53e64d31ecf293
Tharcisse Ntirandekura <tntirand@oberlin.edu>
2025-07-23 21:43:44 -0400

    Fix: Fix register and login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { RouterModule,Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './components/contact/contact.component';
 import { HomeComponent } from './components/home/home.component';
 import { BiochimieComponent } from './components/biochimie/biochimie.component';
@@ -8,7 +8,7 @@ import { ConcoursComponent } from './components/concours/concours.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './components/login/login.component';
 import { ProfileComponent } from './components/profile/profile.component';
-// import { RegisterComponent } from './components/register/register.component';
+import { RegisterComponent } from './components/register/register.component';
 import { NgModule } from '@angular/core';
 import { HelpFormComponent } from './components/help-form/help-form.component';
 import { TermsServiceComponent } from './components/terms/terms-service/terms-service.component';
@@ -18,17 +18,16 @@ export const routes: Routes = [
   { path: 'mathphys', component: MathphysComponent },
   { path: 'biochimie', component: BiochimieComponent },
   { path: 'langues', component: LanguesComponent },
-  { path: 'concours', component: ConcoursComponent},
-  { path: 'help', component: HelpFormComponent},
+  { path: 'concours', component: ConcoursComponent },
+  { path: 'help', component: HelpFormComponent },
   { path: 'contact', component: ContactComponent },
-  {path: 'dashboard',component: DashboardComponent},
-  {path: 'login',component: LoginComponent},
-  {path: 'profile',component: ProfileComponent},
-  {path: 'Terms', component: TermsServiceComponent},
-  {path: 'Privacy', component: TermsServiceComponent},
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'Terms', component: TermsServiceComponent },
+  { path: 'Privacy', component: TermsServiceComponent },
   // {path: 'Terms', component: TermsServiceComponent},
-  // {path: 'register',component: RegisterComponent},
-  
+  { path: 'register', component: RegisterComponent },
   { path: '**', redirectTo: 'home' },
 ];
 
