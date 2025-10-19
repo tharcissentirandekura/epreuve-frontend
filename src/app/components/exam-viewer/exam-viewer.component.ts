af5c5adcb77f23ecf80997fcec796677277153b1
Tharcisse Ntirandekura <tntirand@oberlin.edu>
2025-10-19 15:17:01 -0400

    Add exam viewer template

--- src/app/components/exam-viewer/exam-viewer.component.ts
+++ src/app/components/exam-viewer/exam-viewer.component.ts
@@ -14,9 +14,6 @@ import { Exam } from '../../models/api.model';
 })
 export class ExamViewerComponent implements OnInit {
   exam: Exam | null = null;
-  loading = true;
-  error = '';
-  showAnswers: { [key: number]: { [key: number]: boolean } } = {};
 
   constructor(
     private examService: ExamService,
@@ -29,71 +26,26 @@ export class ExamViewerComponent implements OnInit {
   }
 
   loadExam(): void {
-    this.loading = true;
-    this.error = '';
-    
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.examService.getExamContent(id).subscribe({
       next: (data) => {
         this.exam = data;
-        this.loading = false;
-        this.initializeAnswerVisibility();
       },
       error: (err) => {
-        this.error = 'Erreur de chargement de l\'examen';
-        this.loading = false;
         console.error('Error loading exam:', err);
       }
     });
   }
 
-  retry(): void {
-    this.loadExam();
-  }
-
-  toggleAnswer(questionIndex: number, subQuestionIndex: number): void {
-    if (!this.showAnswers[questionIndex]) {
-      this.showAnswers[questionIndex] = {};
-    }
-    this.showAnswers[questionIndex][subQuestionIndex] = !this.showAnswers[questionIndex][subQuestionIndex];
-  }
-
-  private initializeAnswerVisibility(): void {
-    if (this.exam?.questions) {
-      this.exam.questions.forEach((question, questionIndex) => {
-        this.showAnswers[questionIndex] = {};
-        question.subQuestions?.forEach((_, subQuestionIndex) => {
-          this.showAnswers[questionIndex][subQuestionIndex] = false;
-        });
-      });
-    }
-  }
-
   saveAnswers(): void {
-    // TODO: Implement save functionality
     console.log('Saving answers...');
   }
 
   downloadExam(): void {
-    // TODO: Implement download functionality
     console.log('Downloading exam...');
   }
 
   goBack(): void {
     this.router.navigate(['/']);
-  }
-
-  onTextareaFocus(event: Event): void {
-    const target = event.target as HTMLTextAreaElement;
-    if (target) {
-      target.style.borderColor = '#005ea2';
-    }
-  }
-
-  onTextareaBlur(event: Event): void {
-    const target = event.target as HTMLTextAreaElement;
-    if (target) {
-      target.style.borderColor = '#e5ecf3';
-    }
   }
 }
